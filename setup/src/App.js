5a51299eea796f55fa4ba0816c5fc46d2913814c
CyberFan-EK <merginskiy@gmail.com>
2024-09-17 08:18:08 +0300

    create first lr

---
+++ setup/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const str = "Hello";
+  return (
+    <>
+      <h1>Я натиснув {count} разів на кнопку</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}>
+        ok
+      </button>
+    </>
+  );
+}
+
+export default App;
